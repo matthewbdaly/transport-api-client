b0f58ec29ee919496a587f956f4b4da762cc1665
Matthew Daly <matthewbdaly@gmail.com>
2018-04-30 22:29:52 +0100

    Added HTTP client to the API client

--- spec/ClientSpec.php
+++ spec/ClientSpec.php
@@ -5,9 +5,19 @@ namespace spec\Matthewbdaly\TransportApi;
 use Matthewbdaly\TransportApi\Client;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Http\Client\HttpClient;
+use Http\Message\MessageFactory;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
 
 class ClientSpec extends ObjectBehavior
 {
+    function let (HttpClient $client, MessageFactory $messageFactory)
+    {
+        $this->beConstructedWith($client, $messageFactory);
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType(Client::class);
--- src/Client.php
+++ src/Client.php
@@ -3,7 +3,41 @@
 namespace Matthewbdaly\TransportApi;
 
 use Matthewbdaly\TransportApi\Contracts\Client as ClientInterface;
+use Http\Client\HttpClient;
+use Http\Message\MessageFactory;
+use Http\Discovery\HttpClientDiscovery;
+use Http\Discovery\MessageFactoryDiscovery;
 
+/**
+ * Client for the Transport API
+ */
 class Client implements ClientInterface
 {
+
+    /**
+     * HTTPlug client
+     *
+     * @var $client
+     */
+    protected $client;
+
+    /**
+     * HTTPlug message factory
+     *
+     * @var $messageFactory
+     */
+    protected $messageFactory;
+
+    /**
+     * Constructor
+     *
+     * @param HttpClient     $client         HTTPlug client instance.
+     * @param MessageFactory $messageFactory HTTPlug message factory instance.
+     * @return void
+     */
+    public function __construct(HttpClient $client = null, MessageFactory $messageFactory = null)
+    {
+        $this->client = $client ?: HttpClientDiscovery::find();
+        $this->messageFactory = $messageFactory ?: MessageFactoryDiscovery::find();
+    }
 }
